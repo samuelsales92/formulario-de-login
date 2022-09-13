2735e4bda3931641816d8c03db2b2ad0899ee609
unknown <samuelsales1410@gmail.com>
2022-09-13 05:55:32 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var email = document.getElementById('email');
+
+email.addEventListener('focus',()=>{
+    email.style.borderColor = "#5f7C8A";
+});
+var email = document.getElementById('email');
+
+email.addEventListener('blur',()=>{
+    email.style.borderColor = "#ccc";
+});
+ 
+ 
+
+var password = document.getElementById('password');
+
+password.addEventListener('focus',()=>{
+    password.style.borderColor = "#5f7C8A";
+    
+});
+
+var password= document.getElementById('password');
+
+password.addEventListener('blur',()=>{
+    password.style.borderColor = "#ccc";
+    
+});
+
